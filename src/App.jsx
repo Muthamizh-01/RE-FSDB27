b66c864901d9b9c89bec9990fdc561c8f1a0ef65
Muthamizh-10 <gotitvijay666@gmail.com>
2025-06-23 08:08:22 +0530

    feat: creating first component after project cleanup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// creating our first component
+
+function App() {
+  return (
+    <>
+      <h1> Hello World!!!</h1>
+      <p>welcome all to our webpage</p>
+      <p>onnce againg welcog</p>
+    </>
+  );
+}
+export default App;
