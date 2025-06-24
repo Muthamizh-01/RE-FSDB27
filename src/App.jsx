44f0b9e3e613869eaab8dd5af92159d228aade56
Muthamizh-10 <gotitvijay666@gmail.com>
2025-06-24 14:18:30 +0530

    feat: passing data from child to parent- callback function

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 // using rfce shortcut
 
-import Header from "./components/Header";
+import Hello from "./components/hello";
+
+// import Header from "./components/Header";
 
 function App() {
-  //App: parent component
-  //Header: Child Component
+  // here passing data from child to parent
+  function dataFromChild(name) {
+    console.log("Function inside the parent component");
+    console.log(name);
+  }
 
-  // here name is the attribute
-  // passing data from parent to child
   return (
     <>
-      <Header name={"Krishs"} age={"25"} />
-      <Header name={"srinivas"} age={"30"} />
+      <Hello data={dataFromChild} />
     </>
   );
 }
+//   //App: parent component
+//   //Header: Child Component
+
+//   // here name is the attribute
+//   // passing data from parent to child
+// return (
+//   <>
+//     <Header name={"Krishsann"} age={"25"} />
+//     <Header name={"srinivas"} age={"30"} />
+//   </>
+// );
 
 export default App;
---
+++ src/components/Hello.jsx
@@ -0,0 +1,6 @@
+function Hello(props) {
+  const name = "ABC";
+  props.data(name);
+  return <h1>HEllo World all</h1>;
+}
+export default Hello;
