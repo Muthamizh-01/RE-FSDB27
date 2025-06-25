d62236eacb016b4e2b1c0f084c8c0289af695909
Muthamizh-10 <gotitvijay666@gmail.com>
2025-06-25 16:27:26 +0530

    multiple states

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,20 @@ import { useState } from "react";
 
 const App = () => {
   const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
 
   const handleLike = () => {
     setLikes(likes + 1);
   };
 
   const handleDislike = () => {
-    setLikes(likes - 1);
+    setDislikes(dislikes + 1);
   };
 
   return (
     <div>
       <h2>Likes: {likes} </h2>
+      <h2>Dislikes: {dislikes}</h2>
       <button onClick={handleLike}>Like</button>
       &nbsp;&nbsp;
       <button onClick={handleDislike}>Dislike</button>
