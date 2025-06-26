df221efa84f542c42b7a8f6eef8e628d7471440e
Muthamizh-10 <gotitvijay666@gmail.com>
2025-06-26 14:26:34 +0530

    API calling using useEffecrt

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,19 @@
-// // using rfce shortcut
-// // use rafce shortcut - default arrow function
-// // rce - for class component
+import { useEffect } from "react";
 
-// import { useState } from "react";
-// //import React from "react";
+const App = () => {
+  const fetchTodos = async () => {
+    const response = await fetch(
+      "https://842ae100-b5fa-4e29-9c98-15396ca66193.mock.pstmn.io/products"
+    );
+    const todos = await response.json();
+    console.log(todos);
+  };
 
-// const App = () => {
-//   const [reactions, setReactions] = useState({
-//     likes: 0,
-//     dislikes: 0,
-//   });
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
-//   const handleLike = () => {
-//     setReactions({
-//       ...reactions,
-//       likes: reactions.likes + 1,
-//     });
-//   };
+  return <div>App</div>;
+};
 
-//   const handleDislike = () => {
-//     setReactions({
-//       ...reactions,
-//       dislikes: reactions.dislikes + 1,
-//     });
-//   };
-
-//   console.log(reactions);
-
-//   return (
-//     <div>
-//       <h2>Likes: {likes} </h2>
-//       <h2>Dislikes: {dislikes}</h2>
-//       <button onClick={handleLike}>Like</button>
-//       &nbsp;&nbsp;
-//       <button onClick={handleDislike}>Dislike</button>
-//     </div>
-//   );
-// };
-// export default App;
-
-import { useState } from "react";
-
-export default function App() {
-  const [reactions, setReactions] = useState({ likes: 0, dislikes: 0 });
-
-  const handleLike = () =>
-    setReactions((prev) => ({ ...prev, likes: prev.likes + 1 }));
-
-  const handleDislike = () =>
-    setReactions((prev) => ({ ...prev, dislikes: prev.dislikes + 1 }));
-
-  // ✧ one-liner destructuring:
-  const { likes, dislikes } = reactions;
-
-  return (
-    <div className="p-4 space-x-4">
-      <h2>Likes: {likes}</h2>
-      <h2>Dislikes: {dislikes}</h2>
-
-      <button onClick={handleLike}>Like</button>
-      <button onClick={handleDislike}>Dislike</button>
-    </div>
-  );
-}
+export default App;
