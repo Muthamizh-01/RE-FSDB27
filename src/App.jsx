478753da74327bc6244791cc399bb154dda04a8b
Muthamizh-10 <gotitvijay666@gmail.com>
2025-06-26 14:30:43 +0530

    API calling using then catch

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import { useEffect } from "react";
 
 const App = () => {
-  const fetchTodos = async () => {
-    const response = await fetch(
-      "https://842ae100-b5fa-4e29-9c98-15396ca66193.mock.pstmn.io/products"
-    );
-    const todos = await response.json();
-    console.log(todos);
-  };
-
   useEffect(() => {
-    fetchTodos();
+    fetch("https://842ae100-b5fa-4e29-9c98-15396ca66193.mock.pstmn.io/products")
+      .then((response) => response.json())
+      .then((todos) => console.log(todos));
   }, []);
 
   return <div>App</div>;
